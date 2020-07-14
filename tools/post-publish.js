6b095865ce5db5263264b4adb674af98186128d8
wangshijun <wangshijun2010@gmail.com>
2020-07-14 08:34:52 +0800

    chore: improve publish workflow

---
+++ tools/post-publish.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const axios = require('axios');
+
+(async () => {
+  try {
+    const res = await axios.post('https://api.netlify.com/build_hooks/5d71fd6472feae0bb5d28671');
+    console.log('trigger blocklets build success:', res.status);
+  } catch (error) {
+    console.error('trigger blocklets build failed:', error);
+  }
+})();
